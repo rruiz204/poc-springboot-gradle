03c8178359e64bd76515a67a401efc0831a3f3eb
roney ruiz <roneyruizrojas@gmail.com>
2025-02-14 22:18:50 -0600

    feat: add logger in ping controller

--- src/main/java/com/poc/springboot/controllers/PingController.java
+++ src/main/java/com/poc/springboot/controllers/PingController.java
@@ -1,5 +1,7 @@
 package com.poc.springboot.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,11 @@ import com.poc.springboot.models.Ping;
 @RequestMapping("/ping")
 public class PingController {
 
+  private static final Logger logger = LoggerFactory.getLogger(PingController.class);
+
   @GetMapping
   public Ping ping() {
+    logger.info("logging ping request correctly");
     return new Ping();
   }
 }
